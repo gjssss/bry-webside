f62ad5cc16ff97b334a935f60221acc6fb224e1d
gjssss <gaojinsong2010@gmail.com>
2024-09-07 03:18:39 +0000

    feat: add title

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,8 +5,24 @@
 <template>
   <div>
     <NavComp />
-    <div class="index-bg h-screen w-full">
-      123
+    <div class="index-bg h-screen w-full pl-80px pt-80px">
+      <div class="h-full max-w-564px flex flex-col justify-center c-white">
+        <div class="font-smiley-sans text-64px">
+          We want you:
+        </div>
+        <div class="font-smiley-sans text-64px">
+          我们在这里
+        </div>
+        <div class="font-smiley-sans text-64px">
+          等风也等你
+        </div>
+        <div class="">
+          成立于2000年初，北邮人团队已经陪伴了北邮师生20年有余。我们一直致力于服务北邮人，为北邮师生提供更加便利的服务，一代代北邮人薪火传承北邮人的服务实干精神，用青春书写北邮人的辉煌历程。而现在，无论你是新生还是老司机，只要你怀揣梦想，热爱探索，我们都欢迎你的加入！北邮人总有适合你的地方，让我们一起书写精彩的青春篇章！We Want You！
+        </div>
+        <div>
+          <a class="mt-4 inline-block cursor-pointer rounded-full bg-#fff8 px-8 py-4 backdrop-blur-sm">加入我们</a>
+        </div>
+      </div>
     </div>
     <div class="h-3000px w-full" />
     index
