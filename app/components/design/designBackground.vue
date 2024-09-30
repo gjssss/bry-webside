e84f223b8119c51f4701244f44356c65a1f02f41
gjssss <gaojinsong2010@gmail.com>
2024-09-30 02:30:05 +0000

    feat: add font

--- app/components/design/designBackground.vue
+++ app/components/design/designBackground.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="absolute left-0 top-0 w-full of-hidden text-right text-9xl text-#0067D1 font-800 italic">
+  <div class="absolute left-0 top-0 z-1 h-full w-full flex flex-col justify-between of-hidden text-right text-9xl text-#E5E7EB font-800 italic">
     <designScrollText :factor="1">
       GRAPHIC
     </designScrollText>
--- app/components/design/designShow.vue
+++ app/components/design/designShow.vue
@@ -5,6 +5,17 @@
 <template>
   <div class="relative h-full w-full of-hidden">
     <designBackground />
+    <div class="relative z-2 c-#0067d1">
+      <div class="flex">
+        <img src="/images/departments/design/icon-title.svg" alt="设计">
+        <div>
+          <img src="/images/departments/design/icon-saturn.svg">
+          <div class="font-dfphei text-xl tracking-3.25px sm:text-4xl sm:tracking-5.4px">
+            design=__
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
