6a8eb68088dc0413d5e6a8efc784ad070a127f66
gjssss <gaojinsong2010@gmail.com>
2024-10-02 18:55:18 +0800

    feat: add product show

--- app/components/productShow.vue
+++ app/components/productShow.vue
@@ -3,11 +3,31 @@
 </script>
 
 <template>
-  <div>
-    productShow
+  <div class="bg relative h-full min-h-548px">
+    <div class="absolute bottom-0 mt-auto w-full flex flex-col items-end gap-8 px-[25px] py-[50px]">
+      <div class="bg-text max-w-[528px] w-fit rounded-lg p-[10px] text-lg text-[#333] tracking-[3.6px] sm:text-2xl">
+        我们的日常工作就是负责对产品进行定义并进行需求分析绘制使用流程图，协调技术、设计部门推进项目开发进度。
+      </div>
+      <div class="bg-text w-fit rounded-lg p-[10px] text-lg text-[#333] tracking-[3.6px] sm:text-2xl">
+        收集师生需求&整合各类资源&撰写产品需求文档等相关文字材料
+      </div>
+      <div class="bg-text max-w-[458px] w-fit rounded-lg p-[10px] text-lg text-[#333] tracking-[3.6px] sm:text-2xl">
+        欢迎全新的你加入我们，一起构思新的产品，策划新的活动!
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-
+<style scoped>
+.bg {
+  background-image: url(/images/departments/product/product-bg.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 50%;
+  background-size: 2419px 1524px;
+}
+.bg-text {
+  background-color: hsla(0, 0%, 96%, 0.5);
+  backdrop-filter: blur(4px);
+}
 </style>
